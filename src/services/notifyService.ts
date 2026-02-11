9c5863177f2668370165bd86743b0c3b2520c543
aka-FinGo <akafingo@gmail.com>
2026-02-11 23:21:46 +0500

    Update notifyService.ts

--- src/services/notifyService.ts
+++ src/services/notifyService.ts
@@ -1,5 +1,9 @@
 import { bot } from "../bot";
 
 export async function notifyUser(telegramId: number, text: string) {
-  await bot.telegram.sendMessage(telegramId, text);
+  try {
+    await bot.telegram.sendMessage(telegramId, text);
+  } catch (err) {
+    console.error("Notification error:", err);
+  }
 }
