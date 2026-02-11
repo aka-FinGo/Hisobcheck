e726e4b069ab6f37e04753e68b6bfdcfe51ac021
aka-FinGo <akafingo@gmail.com>
2026-02-12 00:40:23 +0500

    Delete src/routes directory

--- src/routes/auth.ts
+++
@@ -1,7 +0,0 @@
-import { FastifyInstance } from "fastify";
-
-export async function authRoutes(app: FastifyInstance) {
-  app.get("/health", async () => {
-    return { status: "OK" };
-  });
-}
--- src/routes/payments.ts
+++
@@ -1,8 +0,0 @@
-import { FastifyInstance } from "fastify";
-import { createPayment } from "../services/paymentService";
-
-export async function paymentRoutes(app: FastifyInstance) {
-  app.post("/payments", async (req: any) => {
-    return await createPayment(req.body);
-  });
-}
