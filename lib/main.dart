26198c8dec47f4f39f212ad778c1667b58685446
aka-FinGo <akafingo@gmail.com>
2026-02-14 20:07:12 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
+// Bu yerga o'z URL va KEYlaringizni yozing
+const supabaseUrl = 'SU_SIZNING_SUPABASE_URL';
+const supabaseKey = 'SU_SIZNING_ANON_KEY';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // 1. Supabasega ulanish
+  await Supabase.initialize(
+    url: supabaseUrl,
+    anonKey: supabaseKey,
+  );
+
+  runApp(const MyApp());
+}
+
+final supabase = Supabase.instance.client;
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Moliyachi',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        useMaterial3: true,
+      ),
+      // Hozircha bo'sh ekran ko'rsatib turamiz
+      home: const Scaffold(
+        body: Center(child: Text("Supabasega ulandi!")),
+      ),
+    );
+  }
+}
