ba628845d63cb5aa1c74848c949030a6568662da
aka-FinGo <akafingo@gmail.com>
2026-02-14 20:05:43 +0500

    Delete src directory

--- src/services/notifyService.ts
+++
@@ -1,9 +0,0 @@
-import { bot } from "../bot";
-
-export async function notifyUser(telegramId: number, text: string) {
-  try {
-    await bot.telegram.sendMessage(telegramId, text);
-  } catch (err) {
-    console.error("Notification error:", err);
-  }
-}
