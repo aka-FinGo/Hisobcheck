7b022a8b3363a7312ea47215a2c949e4a58adfcc
aka-FinGo <akafingo@gmail.com>
2026-02-15 00:14:35 +0500

    Update home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,19 +10,26 @@ class HomeScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Asosiy Oyna"),
+        title: const Text("Asosiy Sahifa"),
         actions: [
           IconButton(
             onPressed: () async {
               await Supabase.instance.client.auth.signOut();
-              // Saytni yangilash kifoya
-            },
-            icon: const Icon(Icons.logout),
+              // Brauzerni qayta yuklash orqali Login'ga qaytamiz
+            }, 
+            icon: const Icon(Icons.logout)
           )
         ],
       ),
       body: Center(
-        child: Text("Xush kelibsiz!\n${user?.email ?? ''}"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Icon(Icons.check_circle, color: Colors.green, size: 80),
+            const SizedBox(height: 20),
+            Text("Xush kelibsiz!\n${user?.email}", textAlign: TextAlign.center, style: const TextStyle(fontSize: 20)),
+          ],
+        ),
       ),
     );
   }
