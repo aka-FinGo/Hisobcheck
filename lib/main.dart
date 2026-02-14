08dcf580f043691493e2b3a6ce4bb7b164772d5d
aka-FinGo <akafingo@gmail.com>
2026-02-15 00:03:53 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,9 @@ import 'screens/home_screen.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // 1. GitHub Environmentdan kalitlarni olamiz
   const supabaseUrl = String.fromEnvironment('SUPABASE_URL');
   const supabaseKey = String.fromEnvironment('SUPABASE_KEY');
 
-  // Agar kalitlar yo'q bo'lsa (Localda ishlatganda), xato bermasligi uchun tekshiramiz
-  // DIQQAT: O'zingizni lokal kompyuterda ishlatmoqchi bo'lsangiz, shu yerga
-  // haqiqiy URL va KEYni vaqtincha yozib turishingiz mumkin.
   if (supabaseUrl.isNotEmpty && supabaseKey.isNotEmpty) {
     await Supabase.initialize(
       url: supabaseUrl,
@@ -30,17 +26,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Aristokrat Mebel',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue.shade900),
-        useMaterial3: true,
-        inputDecorationTheme: const InputDecorationTheme(
-          border: OutlineInputBorder(),
-          filled: true,
-          fillColor: Colors.white,
-        ),
-      ),
-      // Agar foydalanuvchi tizimda bo'lsa Home, bo'lmasa Login
+      title: 'Hisobcheck',
+      theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.blue),
       home: Supabase.instance.client.auth.currentUser != null
           ? const HomeScreen()
           : const LoginScreen(),
