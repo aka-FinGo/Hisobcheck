fce027180720e9c7bcb391f98e2969ade3aba287
aka-FinGo <akafingo@gmail.com>
2026-02-15 08:43:11 +0500

    Update main_wrapper.dart

--- lib/widgets/main_wrapper.dart
+++ lib/widgets/main_wrapper.dart
@@ -31,3 +31,47 @@ class _MainWrapperState extends State<MainWrapper> {
   }
 
   // --- 2-QISM (UI) PASTDA ---
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // Tanlangan sahifani ko'rsatish
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: _pages,
+      ),
+
+      // --- [ 4. PASTKI MENYU DIZAYNI ] ---
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        type: BottomNavigationBarType.fixed, // 4 tadan ko'p tugma bo'lsa shart
+        selectedItemColor: Colors.blue.shade900,
+        unselectedItemColor: Colors.grey,
+        showUnselectedLabels: true,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.dashboard_outlined),
+            activeIcon: Icon(Icons.dashboard),
+            label: 'Asosiy',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.history_outlined),
+            activeIcon: Icon(Icons.history),
+            label: 'Tarix',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.inventory_2_outlined),
+            activeIcon: Icon(Icons.inventory_2),
+            label: 'Qoldiqlar',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_balance_wallet_outlined),
+            activeIcon: Icon(Icons.account_balance_wallet),
+            label: 'Hamyon',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
